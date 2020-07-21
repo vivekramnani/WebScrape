87d954dfd1d5b1b5135ec4d3e6ee675002152c46
Vivek <vivekramnani@gmail.com>
2020-07-21 18:53:57 +0530

    Files for WebScrape

---
+++ src/webscrape/WebScrape.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webscrape;
+
+/**
+ *
+ * @author iis
+ */
+public class WebScrape {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    }
+    
+}
